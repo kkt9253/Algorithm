0cf0d91b41d86beef99bfe8f9d87904275c7c16b
kkt9253 <71701866+kkt9253@users.noreply.github.com>
2024-07-04 22:33:34 +0900

    [Bronze II] Title: 농구 경기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Bronze/1159. 농구 경기/농구 경기.cc
+++ 백준/Bronze/1159. 농구 경기/농구 경기.cc
@@ -1,33 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-vector<char> checkv;
-vector<char> v;
-string s;
-bool check = false;
+string s, ret;
+int n, cnt[26];
 
 int main() {
-	int n;
 	cin >> n;
 	
 	for (int i = 0; i < n; i++) {
 		cin >> s;
-		v.push_back(s[0]);
+		cnt[s[0] - 'a']++;
 	}
-	sort(v.begin(), v.end());
-	for (char c : v) {
-		auto it = find(checkv.begin(), checkv.end(), c);
-		if (it != checkv.end()) {
-			continue;
-		}
-		int a = lower_bound(v.begin(), v.end(), c) - v.begin();
-		int b = upper_bound(v.begin(), v.end(), c) - v.begin();
-		if (b - a >= 5) {
-			checkv.push_back(c);
-			cout << c;
-			check = true;
-		}
+	
+	for (int i = 0; i < 26; i++) {
+		if (cnt[i] >= 5) ret += (i + 'a');
 	}
-	if (!check) cout << "PREDAJA";
+	if (ret.empty()) cout << "PREDAJA";
+    else cout << ret;
+	
 	return 0;
 }
