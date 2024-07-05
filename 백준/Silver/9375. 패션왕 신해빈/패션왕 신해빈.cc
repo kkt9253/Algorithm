0cae1d40db1df087ad5ba89b4e8717f433cfaa82
kkt9253 <71701866+kkt9253@users.noreply.github.com>
2024-07-05 22:15:23 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.cc
@@ -1,28 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int siz, tc, n;
+int tc, n;
 string a, cate;
-map<string, int> mp;
 
 int main() {
 	cin >> tc;
-	for (int i = 0; i < tc; i++) {
+	while (tc--) {
+		map<string, int> mp;
 		cin >> n;
-		if (n == 0) {
-			cout << "0" << "\n";
-			continue;
-		}
 		for (int j = 0; j < n; j++) {
 			cin >> a >> cate;
 			mp[cate]++;
 		}
-		int prod = 1;
+		long long prod = 1;
 		for (auto ele : mp) {
 			prod *= ele.second + 1;	
 		}
 		cout << prod - 1 << "\n";
-		mp.clear();
 	}
 	
 	return 0;
