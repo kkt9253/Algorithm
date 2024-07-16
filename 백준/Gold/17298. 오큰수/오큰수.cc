8917a0b129093a99c29555fd24d9123db501a3b5
kkt9253 <71701866+kkt9253@users.noreply.github.com>
2024-07-17 00:39:35 +0900

    [Gold IV] Title: 오큰수, Time: 208 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.cc
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define prev tak
+
+int n, a[1000004], ret[1000004];
+stack<int> s;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+	cin >> n;
+	fill(&ret[0], &ret[0] + 1000004, -1);
+	
+	for (int i = 0; i < n; i++) {
+		cin >> a[i];
+		while (s.size() && a[s.top()] < a[i]) {
+			ret[s.top()] = a[i];
+			s.pop();
+		}
+		s.push(i);
+	}
+	
+	for (int i = 0; i < n; i++) cout << ret[i] << " ";
+	
+	return 0;	
+}
