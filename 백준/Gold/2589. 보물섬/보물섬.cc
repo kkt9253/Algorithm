04036883770c3ad7fa2602248a35d1f5589bd917
kkt9253 <71701866+kkt9253@users.noreply.github.com>
2024-07-22 17:28:35 +0900

    [Gold V] Title: 보물섬, Time: 104 ms, Memory: 2048 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.cc
+++ 백준/Gold/2589. 보물섬/보물섬.cc
@@ -5,51 +5,49 @@ using namespace std;
 int dy[] = {-1, 0, 1, 0};
 int dx[] = {0, 1, 0, -1};
 
-int n, m, result = -1, visited[54][54];
-char a[54][54];
+int n, m, result = -1, a[54][54], visited[54][54];
 string s;
 vector<pair<int, int>> ground;
 
 int bfs(int sy, int sx) {
-    fill(&visited[0][0], &visited[0][0] + 54 * 54, 0);
-    queue<pair<int, int>> q;
-    q.push({sy, sx});
-    visited[sy][sx] = 1;
-    int mxdist = 0;
-
-    while (!q.empty()) {
-        int y, x;
-        tie(y, x) = q.front();
-        q.pop();
-
-        for (int i = 0; i < 4; i++) {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            if (ny < 0 || nx < 0 || ny >= n || nx >= m || visited[ny][nx] || a[ny][nx] == 'W') continue;
-            q.push({ny, nx});
-            visited[ny][nx] = visited[y][x] + 1;
-            mxdist = max(mxdist, visited[ny][nx]);
-        }
-    }
-    return mxdist - 1; // 시작점을 포함하므로 1을 뺌
+	fill(&visited[0][0], &visited[0][0] + 54 * 54, 0);
+	queue<pair<int, int>> q;
+	q.push({sy, sx});
+	visited[sy][sx] = 1;
+	int mxdist = -1;
+	
+	while (q.size()) {
+		tie(sy, sx) = q.front();
+		q.pop();
+		for (int i = 0; i < 4; i++) {
+			int ny = sy + dy[i];
+			int nx = sx + dx[i];
+			if (ny < 0 || nx < 0 || ny >= n || nx >= m || visited[ny][nx] || !a[ny][nx]) continue;
+			q.push({ny, nx});
+			visited[ny][nx] = visited[sy][sx] + 1;
+			mxdist = max(mxdist, visited[ny][nx]);
+		}
+	}
+	return mxdist - 1;
 }
 
 int main() {
-    cin >> n >> m;
-
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++) { // n 대신 m
-            cin >> a[i][j];
-            if (a[i][j] == 'L') {
-                ground.push_back({i, j});
-            }
-        }
-    }
-
-    for (auto _ground : ground) {
-        result = max(result, bfs(_ground.first, _ground.second));
-    }
-
-    cout << result << endl;
-    return 0;
+	cin >> n >> m;
+	
+	for (int i = 0; i < n; i++) {
+		cin >> s;
+		for (int j = 0; j < m; j++) {
+			if (s[j] == 'W') a[i][j] = 0;
+			if (s[j] == 'L') {
+				a[i][j] = 1;
+				ground.push_back({i, j});
+			}
+		}
+	}
+	for (auto _ground : ground) {
+		result = max(result, bfs(_ground.first, _ground.second));
+	}
+	
+	cout << result;
+	return 0;
 }
