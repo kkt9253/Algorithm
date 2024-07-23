396a61ce4ab07e0326977c0c19a2504e062d6f78
kkt9253 <71701866+kkt9253@users.noreply.github.com>
2024-07-24 01:43:01 +0900

    [Gold III] Title: 불!, Time: 52 ms, Memory: 11016 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.cc
+++ 백준/Gold/4179. 불！/불！.cc
@@ -7,66 +7,65 @@ int dy[] = {-1, 0, 1, 0};
 int dx[] = {0, 1, 0, -1};
 
 char a[1004][1004];
-int n, m;
-int ft[1004][1004], jh[1004][1004];
+int n, m, ft[1004][1004], jh[1004][1004];
 queue<pair<int, int>> f;
 queue<pair<int, int>> jq;
 
 int main() {
-    ios_base::sync_with_stdio(false);
+	ios_base::sync_with_stdio(false);
     cin.tie(NULL); cout.tie(NULL);
     
     cin >> n >> m;
+    fill(&ft[0][0], &ft[0][0] + 1004 * 1004, INF);
+    fill(&jh[0][0], &jh[0][0] + 1004 * 1004, INF);
+    
     for(int i = 0; i < n; i++){ 
         for(int j = 0; j < m; j++){
             cin >> a[i][j];
             if (a[i][j] == 'F') {
-                ft[i][j] = 0;
-                f.push({i, j});
-            } else {
-                ft[i][j] = INF;
-            }
+            	ft[i][j] = 0;
+            	f.push({i, j});
+			}
             if (a[i][j] == 'J') {
-                jh[i][j] = 0;
-                jq.push({i, j});
-            } else {
-                jh[i][j] = INF;
-            }
+            	jh[i][j] = 0;
+            	jq.push({i, j});
+			}
         }
     }
     
-    while(!f.empty()) {
-        int y = f.front().first;
-        int x = f.front().second;
-        f.pop();
-        for (int i = 0; i < 4; i++) {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
-            if (ft[ny][nx] != INF || a[ny][nx] == '#') continue;
-            f.push({ny, nx});
-            ft[ny][nx] = ft[y][x] + 1;
-        }
-    }
-    
-    while(!jq.empty()) {
-        int y = jq.front().first;
-        int x = jq.front().second;
-        jq.pop();
-        if (x == m - 1 || y == n - 1 || x == 0 || y == 0) {
-            cout << jh[y][x] + 1;
-            return 0;
-        }
-        for (int i = 0; i < 4; i++) {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
-            if (a[ny][nx] == '#' || jh[ny][nx] != INF || jh[y][x] + 1 >= ft[ny][nx]) continue;
-            jq.push({ny, nx});
-            jh[ny][nx] = jh[y][x] + 1;
-        }
-    }
+    while(f.size()) {
+    	int y = f.front().first;
+    	int x = f.front().second;
+    	f.pop();
+    	for (int i = 0; i < 4; i++) {
+    		int ny = y + dy[i];
+    		int nx = x + dx[i];
+    		if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+    		if (ft[ny][nx] != INF || a[ny][nx] == '#') continue;
+    		f.push({ny, nx});
+    		ft[ny][nx] = ft[y][x] + 1;
+		}
+	}
+	
+	
+	while(jq.size()) {
+    	int y = jq.front().first;
+    	int x = jq.front().second;
+    	jq.pop();
+		if (x == m - 1 || y == n - 1 || x == 0 || y == 0) {
+			cout << jh[y][x] + 1;
+        	return 0;
+		}
+    	for (int i = 0; i < 4; i++) {
+    		int ny = y + dy[i];
+    		int nx = x + dx[i];
+    		if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+    		if (a[ny][nx] == '#' || jh[y][x] + 1 >= ft[ny][nx] || jh[ny][nx] != INF) continue;
+    		jq.push({ny, nx});
+    		jh[ny][nx] = jh[y][x] + 1;
+		}
+	}
     
     cout << "IMPOSSIBLE";
-    return 0;  
+	return 0;	
 }
