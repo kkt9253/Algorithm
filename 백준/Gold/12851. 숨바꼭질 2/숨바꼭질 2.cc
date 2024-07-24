9f3500f20d4cf8ed2c7f1fd1538e0fdaae8b77bf
kkt9253 <71701866+kkt9253@users.noreply.github.com>
2024-07-25 01:04:22 +0900

    [Gold IV] Title: 숨바꼭질 2, Time: 0 ms, Memory: 3324 KB -BaekjoonHub

--- 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
+++ 백준/Gold/12851. 숨바꼭질 2/숨바꼭질 2.cc
@@ -1,42 +1,44 @@
 #include <bits/stdc++.h>
 using namespace std;
-const int MAX = 200000;
-int visited[MAX + 4];
-long long cnt[MAX + 4];
+const int INF = 1e9;
+
+int dy[] = {-1, 0, 1, 0};
+int dx[] = {0, 1, 0, -1};
+
+long long cnt[100004];
+int n, k, visited[100004];
+queue<int> q;
 
 int main() {
-    int n, k;
-    cin >> n >> k;
-    
-    if (n == k) {
-        puts("0"); 
-        puts("1");
-        return 0; 
-    }
-
-    visited[n] = 1;
-    cnt[n] = 1;
-    queue<int> q;
-    q.push(n);
-    
-    while (!q.empty()) {
-        int now = q.front();
-        q.pop();
-        
-        for (int next : {now - 1, now + 1, now * 2}) {
-            if (0 <= next && next <= MAX) {
-                if (!visited[next]) {
-                    q.push(next);
-                    visited[next] = visited[now] + 1;
-                    cnt[next] = cnt[now];  // 새로 방문한 경우
-                } else if (visited[next] == visited[now] + 1) {
-                    cnt[next] += cnt[now];  // 최단 시간으로 방문한 경우
-                }
-            }
-        }
-    }
-    
-    cout << visited[k] - 1 << '\n';
-    cout << cnt[k] << '\n';
-    return 0;
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> n >> k;
+	
+	if (n == k) {
+		cout << 0 << "\n" << 1;
+		return 0;
+	}
+	
+	visited[n] = 1;
+	cnt[n] = 1;
+	q.push(n);
+	while(q.size()) {
+		int now = q.front();
+		q.pop();
+		
+		for (int next : {now + 1, now - 1, now * 2}) {
+			if (0 <= next && next < 100004) {
+				if (!visited[next]) {
+					q.push(next);
+					visited[next] = visited[now] + 1;
+					cnt[next] = cnt[now];
+				}
+				else if (visited[next] == visited[now] + 1) cnt[next] += cnt[now];
+			}
+		}
+	}
+	
+	cout << visited[k] - 1 << "\n" << cnt[k];
+	return 0;
 }
