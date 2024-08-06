eb66feeb3417f3408484280fcfc5fabcbc64d19e
kkt9253 <71701866+kkt9253@users.noreply.github.com>
2024-08-06 19:26:16 +0900

    [Gold IV] Title: 알파벳, Time: 364 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.cc
+++ 백준/Gold/1987. 알파벳/알파벳.cc
@@ -1,41 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int INF = 1e9;
 
 int dy[] = {-1, 0, 1, 0};
 int dx[] = {0, 1, 0, -1};
 
+int n, m, visited, ret = -1;
 char a[24][24];
-int n, m, visited[30], ret;
+string s;
 
-void go(int sy, int sx, int cnt) {
+void dfs(int sy, int sx, int cnt) {
 	ret = max(ret, cnt);
+	
 	for (int i = 0; i < 4; i++) {
 		int ny = sy + dy[i];
 		int nx = sx + dx[i];
-		if (0 > nx || 0 > ny || nx >= m || ny >= n) continue;
-		int next = a[ny][nx] - 'A';
-		if (!visited[next]) {
-			visited[next] = 1;
-			go(ny, nx, cnt + 1);
-			visited[next] = 0;
-		}
+		if (ny < 0 || nx < 0 || ny >= n || nx >= m) continue;
+		if (visited & (1 << (a[ny][nx] - 'A'))) continue;
+		visited |= (1 << (a[ny][nx] - 'A'));
+		dfs(ny, nx, cnt + 1);
+		visited &= ~(1 << (a[ny][nx] - 'A'));
 	}
 }
 
 int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    
-    cin >> n >> m;
-    for (int i = 0; i < n; i++) {
-    	for (int j = 0; j < m; j++) {
-    		cin >> a[i][j];
+	ios_base::sync_with_stdio(false);
+	cin.tie(0); cout.tie(0);
+	
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		cin >> s;
+		for (int j = 0; j < m; j++) {
+			a[i][j] = s[j];
 		}
 	}
-	visited[a[0][0] - 'A'] = 1;
-	go(0, 0, 1);
+	visited |= (1 << (a[0][0] - 'A'));
+	dfs(0, 0, 1);
 	
 	cout << ret;
-    return 0;
+	
+	return 0;
 }
