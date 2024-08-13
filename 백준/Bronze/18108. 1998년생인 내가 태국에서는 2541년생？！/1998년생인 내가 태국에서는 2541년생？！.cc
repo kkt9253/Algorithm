6406ac37e11ef62fbbca222e0988e1b955e9c4d4
kkt9253 <71701866+kkt9253@users.noreply.github.com>
2024-08-13 16:19:08 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int a;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> a;
+	cout << (a - 543);
+	return 0;
+}
