82cd0ed9d5a20e7eee785cca24b87a415e051110
kkt9253 <71701866+kkt9253@users.noreply.github.com>
2024-08-20 16:24:30 +0900

    [Silver V] Title: 피카츄, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/14405. 피카츄/피카츄.cc
+++ 백준/Silver/14405. 피카츄/피카츄.cc
@@ -2,45 +2,24 @@
 using namespace std;
 
 bool flag;
-int n;
-string a[3] = {"pi", "ka", "chu"};
 string s;
-queue<char> q;
-
-bool solve(string ss) {
-	for (char c : ss) {
-		if (q.front() == c) q.pop();
-		else return false;
-	}
-	return true;
-}
 
 int main() {
 	ios_base::sync_with_stdio(false);
 	cin.tie(NULL); cout.tie(NULL);
 	
 	cin >> s;
-	for (char c : s) q.push(c);
 	
-	while (q.size()) {
-		if (q.size() >= 2 && q.front() == 'p') {
-		    flag = solve(a[0]);
-		}
-		else if (q.size() >= 2 && q.front() == 'k') {
-		    flag = solve(a[1]);
-		}
-		else if (q.size() >= 3 && q.front() == 'c') {
-		    flag = solve(a[2]);
-		}
+	for (int i = 0; i < s.size(); i++) {
+		if (i < (s.size() - 1) && (s.substr(i, 2) == "pi" || s.substr(i, 2) == "ka")) i += 1;
+		else if (i < (s.size() - 2) && s.substr(i, 3) == "chu") i += 2;
 		else {
-			flag = false;
+			flag = 1;
 			break;
 		}
-		if (!flag) break;
 	}
-	
-	if (flag) cout << "YES";
-	else cout << "NO";
+	if (flag) cout << "NO";
+	else cout << "YES";
 	
 	return 0;
 }
