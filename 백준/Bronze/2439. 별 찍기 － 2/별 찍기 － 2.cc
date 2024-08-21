d07e0d44d3e2dd7d80d30fcd08bee53007fc66db
kkt9253 <71701866+kkt9253@users.noreply.github.com>
2024-08-21 17:13:00 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.cc
@@ -10,12 +10,7 @@ int main() {
 	cin >> n;
 	
 	for (int i = 0; i < n; i++) {
-		int start = n - i - 1;
-		for (int j = 0; j < n; j++) {
-			if (j < start) cout << " ";
-			else cout << "*";
-		}
-		cout << "\n";
+		cout << string(n - i - 1, ' ') << string(i + 1, '*') << "\n";
 	}
 	
 	
