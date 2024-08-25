21db138b9711ef1286c1f08a99e91a74c9bebbf9
kkt9253 <71701866+kkt9253@users.noreply.github.com>
2024-08-25 15:50:22 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF 1e9
+
+string s;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> s;
+	
+	cout << s.size();
+	
+	return 0;
+}
