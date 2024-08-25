4b7ea0151e6ed1eec53db4ac28123ac2cc1cac1e
kkt9253 <71701866+kkt9253@users.noreply.github.com>
2024-08-25 15:48:25 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF 1e9
+
+string s;
+int n;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> s >> n;
+	
+	cout << s[n - 1];
+	
+	return 0;
+}
