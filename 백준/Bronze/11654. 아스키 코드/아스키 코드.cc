71b3ad4e4886a1269511982766a731bda8d18419
kkt9253 <71701866+kkt9253@users.noreply.github.com>
2024-08-25 15:57:04 +0900

    [Bronze V] Title: 아스키 코드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define INF 1e9
+
+char s;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL); cout.tie(NULL);
+	
+	cin >> s;
+	cout << int(s);
+	
+	
+	return 0;
+}
