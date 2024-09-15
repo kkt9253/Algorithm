acaeec44e00b954f2c90b3dc4114a8b42a43db49
kkt9253 <71701866+kkt9253@users.noreply.github.com>
2024-09-15 18:03:25 +0900

    [Silver I] Title: 영역 구하기, Time: 0 ms, Memory: 2448 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.cc
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.cc
@@ -1,46 +1,53 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define INF 1e9
+typedef long long ll;
 
-int dy[] = {-1, 0, 1, 0}, dx[] = {0, 1, 0, -1};
-int cnt, dfscnt, n, m, k, adj[104][104], visited[104][104];
+int dy[4] = {1, 0, -1, 0};
+int dx[4] = {0, 1, 0, -1};
+int m, n, k, a, b, c, d, ar[104][104], visited[104][104];
 vector<int> ret;
 
-int dfs(int y, int x) {
-	visited[y][x] = 1;
-	int ret = 1;
-	
-	for (int i = 0; i < 4; i++) {
-		int ny = y + dy[i];
-		int nx = x + dx[i];
-		if (0 > ny || 0 > nx || m <= ny || n <= nx) continue;
-		if (!visited[ny][nx] && !adj[ny][nx]) {
-			ret += dfs(ny, nx);
-		}
-	}
-	return ret;
+int dfs(int sy, int sx) {
+    int siz = 1;
+    visited[sy][sx] = 1;
+    
+    for (int i = 0; i < 4; i++) {
+        int ny = sy + dy[i];
+        int nx = sx + dx[i];
+        if (ny < 0 || ny >= m || nx < 0 || nx >= n || ar[ny][nx] || visited[ny][nx]) continue;
+        siz += dfs(ny, nx);
+    }
+    
+    return siz;
 }
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
     cin >> m >> n >> k;
-    int lx, ly, rx, ry;
+    
     while (k--) {
-        cin >> lx >> ly >> rx >> ry;
-        for (int i = ly; i < ry; i++) {
-            for (int j = lx; j < rx; j++) {
-                adj[m - 1 - i][j] = 1;
+        cin >> a >> b >> c >> d;
+        
+        for (int i = b; i < d; i++) {
+            for (int j = a; j < c; j++) {
+                ar[i][j] = 1;
             }
         }
     }
     
     for (int i = 0; i < m; i++) {
         for (int j = 0; j < n; j++) {
-        	if (!visited[i][j] && !adj[i][j]) {
-        		ret.push_back(dfs(i, j));
-			}
+            if (!ar[i][j] && !visited[i][j]) {
+                int area_size = dfs(i, j);
+                ret.push_back(area_size);
+            }
         }
     }
     
     sort(ret.begin(), ret.end());
+    
     cout << ret.size() << "\n";
     for (int i : ret) cout << i << " ";
     
