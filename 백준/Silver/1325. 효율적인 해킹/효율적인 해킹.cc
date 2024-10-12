bdf95f02bb45f2ed9cc115232f1f1359194f9520
kkt9253 <71701866+kkt9253@users.noreply.github.com>
2024-10-13 02:49:46 +0900

    [Silver I] Title: 효율적인 해킹, Time: 2008 ms, Memory: 3468 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.cc
@@ -6,53 +6,46 @@ vector<int> v[10004];
 vector<int> ret;
 
 int bfs(int start) {
-    int cnt = 0;
-    vector<bool> visited(n + 1, false);
-    queue<int> q;
-    q.push(start);
-    visited[start] = true;
-
-    while (!q.empty()) {
-        int cur = q.front();
-        q.pop();
-        cnt++;
-
-        for (int i : v[cur]) {
-            if (!visited[i]) {
-                q.push(i);
-                visited[i] = true;
-            }
-        }
-    }
-
-    return cnt;
+	int cnt = 0;
+	queue<int> q;
+	q.push(start);
+	int visited[10004] = {0};
+	visited[start] = 1;
+	
+	while (q.size()) {
+		start = q.front(); q.pop();
+		cnt++;
+		for (int i : v[start]) {
+			if (visited[i]) continue;
+			q.push(i);
+			visited[i] = 1;
+		}
+	}
+	
+	return cnt;
 }
 
 int main() {
     ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
+    cin.tie(NULL); cout.tie(NULL);
+    
     cin >> n >> m;
     for (int i = 0; i < m; i++) {
-        cin >> a >> b;
-        v[b].push_back(a);
-    }
-
-    for (int i = 1; i <= n; i++) {
-        int cnt = bfs(i);
-        if (cnt > mx) {
-            mx = cnt;
-            ret.clear();
-            ret.push_back(i);
-        } else if (cnt == mx) {
-            ret.push_back(i);
-        }
-    }
-
-    for (int i : ret) {
-        cout << i << " ";
-    }
+    	cin >> a >> b;
+    	v[b].push_back(a);
+	}
+	
+	for (int i = 1; i <= n; i++) {
+		int cnt = bfs(i);
+		if (cnt > mx) {
+			mx = cnt;
+			ret.clear();
+			ret.push_back(i);
+		}
+		else if (cnt == mx) ret.push_back(i);
+	}
+	
+	for (int i : ret) cout << i << " ";
 
     return 0;
 }
