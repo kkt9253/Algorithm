f60952cf7625e2dd0254dc2b00ccff86f39d0fed
kkt9253 <71701866+kkt9253@users.noreply.github.com>
2024-11-15 18:31:53 +0900

    [Gold III] Title: 불!, Time: 548 ms, Memory: 78728 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.java
+++ 백준/Gold/4179. 불！/불！.java
@@ -1,5 +1,7 @@
 import java.io.*;
-import java.util.*;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
     static final int INF = Integer.MAX_VALUE;
@@ -13,14 +15,16 @@ public class Main {
 
     static Queue<Point> qfire = new LinkedList<>();
     static Point start;
-    static int r, c;
 
-    static int[] dy = {1, 0, -1, 0};
-    static int[] dx = {0, 1, 0, -1};
+    static int r;
+    static int c;
+
+    static int dy[] = {1, 0, -1, 0};
+    static int dx[] = {0, 1, 0, -1};
 
     static class Point {
-        private int y;
-        private int x;
+        int y;
+        int x;
 
         public Point(int y, int x) {
             this.y = y;
@@ -36,42 +40,37 @@ public class Main {
         adj = new int[r][c];
         fire = new int[r][c];
         visited = new boolean[r][c];
-
         for (int[] row : fire) Arrays.fill(row, INF);
+        for (int[] row : adj) Arrays.fill(row, -1);
 
         for (int i = 0; i < r; i++) {
             String ss = br.readLine();
             for (int j = 0; j < c; j++) {
-                if (ss.charAt(j) == '.') {
-                    adj[i][j] = 0;
-                } else if (ss.charAt(j) == 'J') {
+                if (ss.charAt(j) == '.') adj[i][j] = 0;
+                else if (ss.charAt(j) == 'J') {
                     adj[i][j] = 0;
                     start = new Point(i, j);
-                } else if (ss.charAt(j) == 'F') {
+                }
+                else if (ss.charAt(j) == 'F') {
                     qfire.offer(new Point(i, j));
                     fire[i][j] = 0;
-                } else if (ss.charAt(j) == '#') {
-                    adj[i][j] = -1;
                 }
 
-                // 경계에 지훈이 시작 위치가 있는 경우 즉시 탈출
                 if ((i == 0 || i == r - 1 || j == 0 || j == c - 1)) {
                     if (ss.charAt(j) == 'J') {
-                        bw.write("1\n");
+                        bw.write("1");
                         bw.flush();
-                        return;
+                        return ;
                     }
                 }
             }
         }
 
-        // 불 BFS
-        bfsFire();
-
-        // 지훈이 BFS
+        bfs();
+        // 지훈이 bfs 돌리기
+        visited[start.y][start.x] = true;
         Queue<Point> q = new LinkedList<>();
         q.offer(start);
-        visited[start.y][start.x] = true;
 
         while (!q.isEmpty()) {
             Point point = q.poll();
@@ -82,28 +81,27 @@ public class Main {
                 int ny = sy + dy[i];
                 int nx = sx + dx[i];
 
-                // 경계에 도달한 경우
                 if (ny < 0 || nx < 0 || ny >= r || nx >= c) {
-                    bw.write((adj[sy][sx] + 1) + "\n");
+                    bw.write(adj[sy][sx] + 1 + "\n");
                     bw.flush();
                     return;
                 }
 
-                // 방문 조건 확인
-                if (adj[ny][nx] == -1 || visited[ny][nx] || fire[ny][nx] <= adj[sy][sx] + 1) continue;
-
+                if (visited[ny][nx] || adj[ny][nx] == -1 || fire[ny][nx] <= adj[sy][sx] + 1) continue;
                 visited[ny][nx] = true;
                 adj[ny][nx] = adj[sy][sx] + 1;
                 q.offer(new Point(ny, nx));
             }
         }
 
-        // 탈출 불가능한 경우
         bw.write("IMPOSSIBLE\n");
+
         bw.flush();
+        br.close();
+        bw.close();
     }
 
-    static void bfsFire() {
+    static void bfs() {
         while (!qfire.isEmpty()) {
             Point point = qfire.poll();
             int sy = point.y;
