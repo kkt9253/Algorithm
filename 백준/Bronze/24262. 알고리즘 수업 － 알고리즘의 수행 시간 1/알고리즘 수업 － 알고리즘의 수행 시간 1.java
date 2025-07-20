2f3acee69e3013abf3d338165ba38164d5a7948a
kkt9253 <71701866+kkt9253@users.noreply.github.com>
2025-07-20 23:49:31 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 104 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.ArrayList;
+
+public class Main {
+	
+	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    public static void main(String[] args) throws IOException {
+    	
+    	bw.write("1\n");
+    	bw.write("0\n");
+    	bw.flush();
+    	bw.close();
+    	
+    }
+}
