032ed2ea3605aad5f0dacd225c927b82a118c4f9
kkt9253 <71701866+kkt9253@users.noreply.github.com>
2025-07-25 15:43:00 +0900

    [Bronze IV] Title: 베라의 패션, Time: 116 ms, Memory: 15776 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+public class Main {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws IOException {
+
+        int n = Integer.parseInt(br.readLine());
+        bw.write(n*n-n + "\n");
+
+        bw.flush();
+        bw.close();
+
+    }
+}
